5d25ad22ac7b8826120522546ab510769a3ab9e3
Lin <lincarbajales@gmail.com>
2025-10-07 09:47:51 +0200

    first commit

---
+++ src/main/java/dev/lin/exquis/ExquisApplication.java
@@ -0,0 +1,13 @@
+package dev.lin.exquis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExquisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExquisApplication.class, args);
+	}
+
+}
