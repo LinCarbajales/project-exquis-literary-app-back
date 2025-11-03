3d17ed5623f4913c11c56924379355501e458c3a
Lin <lincarbajales@gmail.com>
2025-11-03 14:50:21 +0100

    superfluous import and file deleted

--- src/main/java/dev/lin/exquis/ExquisApplication.java
+++ src/main/java/dev/lin/exquis/ExquisApplication.java
@@ -2,7 +2,6 @@ package dev.lin.exquis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
